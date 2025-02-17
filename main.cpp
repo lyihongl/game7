1dabf84eea52f0775c3fde79a27d60336dc3858f
Yi Hong Liu <yihongliu00@gmail.com>
2025-02-16 22:01:55 -0800

    WIP turret

--- main.cpp
+++ main.cpp
@@ -33,15 +33,29 @@ struct turret
     std::optional<std::vector<mounting_point>::iterator> attachment_point;
     turret()
       : attachment_point(std::nullopt)
+      , gun_angle(0)
     {
     }
+    float gun_angle;
 };
+
+void
+draw_turret(const turret& t, const Vector2& mouse_pos)
+{
+    if (t.attachment_point.has_value()) {
+
+    } else {
+        auto [cx, cy] = mouse_pos;
+        DrawCircleLines(cx, cy, 10, BLACK);
+        DrawLine(cx, cy, cx, cy - 15, RED);
+    }
+}
 
 struct ship
 {
     double angle;
-    int x;
-    int y;
+    float x;
+    float y;
     std::vector<mounting_point> mounting_points;
     Vector2 get_center() const;
 };
@@ -69,31 +83,59 @@ draw_ship(const ship& s)
     }
 }
 
+std::ostream&
+operator<<(std::ostream& os, const Vector2& c)
+{
+    os << "Vector2(" << c.x << ", " << c.y << ")";
+    return os;
+}
+
+Vector2
+operator+(Vector2& s, const Vector2& other)
+{
+    return Vector2Add(s, other);
+}
+
+Vector2
+operator-(Vector2& s, const Vector2& other)
+{
+    return Vector2Subtract(s, other);
+}
+
 int
 main(void)
 {
     ship s{ .angle = 0, .x = 100, .y = 100 };
+    ship y{ .angle = 0, .x = 200, .y = 100 };
     s.mounting_points.emplace_back(
       mounting_point{ .x_offset = 20, .y_offset = 10 });
 
     s.mounting_points.emplace_back(
       mounting_point{ .x_offset = -20, .y_offset = 10 });
 
+    turret t;
+
     InitWindow(1920, 1080, "raylib [core] example - basic window");
     Camera2D c{};
     c.zoom = 1.0f;
     c.offset = { 1920.f / 2, 1080.f / 2 };
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
 
         c.zoom += ((float)GetMouseWheelMove() * 0.2f);
         c.target = s.get_center();
+        std::cout << c.target << std::endl;
+        s.y += 1;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         {
             BeginMode2D(c);
             draw_ship(s);
+            draw_ship(y);
+            draw_turret(t, GetMousePosition() + c.target - c.offset);
             EndMode2D();
         }
 
